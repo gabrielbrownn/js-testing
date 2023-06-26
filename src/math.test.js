5a6135f0f7b38098a6bc97d6c43cb8fac0321b28
Gabriel Brown <brown.nokaya@gmail.com>
2023-06-26 13:10:21 -0300

    Adding more tests to math.js

--- src/math.test.js
+++ src/math.test.js
@@ -14,3 +14,40 @@ test("should summarize all numbers values in an array", () => {
   //Assert
   expect(result).toBe(expectedResult);
 });
+
+test("should return NaN if at least one invalid number is provide", () => {
+  const inputs = ["invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+test("should return a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
+
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0
+  );
+
+  const result = add(numbers);
+
+  expect(result).toBe(expectedResult);
+});
+
+test("should return 0 if an empty array is provided", () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+test("should throw an error if no value is passed into the function", () => {
+  const resultFn = () => {
+    add();
+  };
+
+  expect(resultFn).toThrow();
+});
