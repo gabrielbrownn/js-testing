14731f6bd464e1bbd401a12664a4996b0a1abbf7
Gabriel Brown <brown.nokaya@gmail.com>
2023-06-27 14:17:06 -0300

    Add new test

--- src/math.test.js
+++ src/math.test.js
@@ -49,5 +49,16 @@ test("should throw an error if no value is passed into the function", () => {
     add();
   };
 
-  expect(resultFn).toThrow();
+  expect(resultFn).toThrow(/is not iterable/);
+});
+
+test("should throw an error if provided with multiple arguments instead of an array", () => {
+  const num1 = 1;
+  const num2 = 2;
+
+  const resultFn = () => {
+    add(num1, num2);
+  };
+
+  expect(resultFn).toThrow(/is not iterable/);
 });
